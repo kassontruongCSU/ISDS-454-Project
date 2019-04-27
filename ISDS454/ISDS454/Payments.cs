a3f22022aa48c196f08db725ffa61f54b7e64b40
kassontruongCSU <36388815+kassontruongCSU@users.noreply.github.com>
2019-04-27 15:30:37 -0700

    Add files via upload

--- ISDS454/ISDS454/Payments.cs
+++ ISDS454/ISDS454/Payments.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ISDS454
 {
     public partial class Payments : Form
     {
+        string strApartFind;
+
         public Payments()
         {
             InitializeComponent();
+            
+            
+
+        }
+
+        private void Payments_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            strApartFind = textBox1.Text;
+            MessageBox.Show(strApartFind, "has not paid");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
